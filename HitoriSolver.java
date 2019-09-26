1a39aad5510f7ec79ce541f3de1c18ee2d1d4ff4
Simen Osland Romstad <31279006+SimenRom@users.noreply.github.com>
2019-09-26 22:20:18 +0200

    Formatere syntaks

--- HitoriSolver.java
+++ HitoriSolver.java
@@ -1,28 +1,30 @@
 public class rute{
 	Integer verdi;
-  boolean markert;
+  	boolean markert;
   
   public rute(Integer verdi){
   	this.verdi = verdi;
-    merkert = false;
+    	merkert = false;
   }
   
-  pubic marker(){
+  pubic void marker(){
   	markert = true;
   }
   
 }
-public static class hitoriSolver {
-	public static main(String[] args){}
+public static class HitoriSolver {
+	public static main(String[] args){
+	//Skriv inn ditt hitori-brett i todimensjonell tabell og køyr metoden hitoriInitSolve().
+	}
 
 	int strl = 5;
 	rute[][] brett = new rute[strl][strl];
 
 
 	public int[][] hitoriInitSolve(int[][] brett){
-  	if(sjekkGyldigBrett(brett)){
+  		if(sjekkGyldigBrett(brett)){
 			return hitoriStartSolve(brett);
-  	}  
+  		}  
 	}
   
   public boolean hitoriStartSolve(brett){
@@ -31,19 +33,17 @@ public static class hitoriSolver {
   
   public boolean sjekkGyldigBrett(int[][] brett){
   	return (
-    sjekkGyldigMarkeringer(brett) &&
-    sjekkTraverserbar(brett) &&
-    sjekkRader(brett) &&
-    sjekkKolonner(brett)
+    		sjekkGyldigMarkeringer(brett) &&
+		sjekkTraverserbar(brett) &&
+    		sjekkRader(brett) &&
+    		sjekkKolonner(brett)
   	);
   }
 
-	public boolean sjekkHøgreNabo(int x, int y){
-		if(brett[x][y+1].getMarkert()){
-    	
-    }
-	}
-
+  public boolean sjekkHøgreNabo(int x, int y){
+	if(brett[x][y+1].getMarkert()){}
+	  //not done
+  }
 }
 
 
